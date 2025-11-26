3543cb6c76f52c81f5a3245084698ddbbe0c8c2a
Kirthiga R <1234kirthik@gmail.com>
2025-11-26 17:30:00 +0530

    Change DATABASE_URL to use environment variable
    Updated DATABASE_URL to use environment variable.

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "sqlite:///./users.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 Base = declarative_base()
+
